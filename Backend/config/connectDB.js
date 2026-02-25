855e0615c0de1ffa2c24ac73a6f23e073634259c
deepanshu yadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2026-02-25 13:55:00 +0530

    remove the comment lines

--- Backend/config/connectDB.js
+++ Backend/config/connectDB.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // await mongoose.connect("mongodb://127.0.0.1:27017/patientList");
     await mongoose.connect(`${process.env.MONGODB_URI}/gateugcnetdata`)
     console.log("MongoDB connected ✅");
   } catch (error) {
